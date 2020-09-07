3ea84c7a586f7224a8110e9bd61e4629d4d9e68a
jothamardel <ardelmbiplang@gmail.com>
2020-09-07 10:34:31 +0100

    Implemented sign in component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/Home-page/homepage.component';
+import SigIn from './pages/SignIn-Page/sign-in.container';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* <HomePage /> */}
+      <SigIn />
     </div>
   );
 }
--- src/pages/SignIn-Page/sign-in.container.jsx
+++ src/pages/SignIn-Page/sign-in.container.jsx
@@ -7,12 +7,26 @@ import './sign-in.styles.scss';
 class SigIn extends React.Component {
   render() {
     return (
-      <div>
-
+      <div className="sign-in">
+        <div className="sign-in-container">
+          <h1>Sign In</h1>
+          <form action="">
+            <input type="text" placeholder="Phone Number" name="mobile" />
+            <input type="password" name="password" id="" />
+            <button type="submit">Login</button>
+          </form>
+          <p>Don't have an account? Sign Up.</p>
+          <hr></hr>
+          <p>or</p>
+          <div className="oauth">
+            <button><i class="fab fa-facebook-square"></i>Login with Google</button>
+            <button><i class="fab fa-google"></i>Login with Facebook</button>
+          </div>
+        </div>
+        <img src={require("../../assets/market_woman1.jpg")} alt="Market woman" />
       </div>
     );
   }
 }
-
 
 export default SigIn;
