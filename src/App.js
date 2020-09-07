a64bebd61592d3ceb0ece6e0d2430e405c2b282a
jothamardel <ardelmbiplang@gmail.com>
2020-09-07 10:55:37 +0100

    Implemented sign up component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/Home-page/homepage.component';
 import SigIn from './pages/SignIn-Page/sign-in.container';
+import SignUp from './pages/SignUp-Page/sign-up.component';
 
 function App() {
   return (
     <div className="App">
       {/* <HomePage /> */}
-      <SigIn />
+      {/* <SigIn /> */}
+      <SignUp />
     </div>
   );
 }
---
+++ src/pages/SignUp-Page/sign-up.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './sign-up.styles.scss';
+
+
+class SignUp extends React.Component {
+  render() {
+    return (
+      <div className="sign-up">
+        <div className="sign-up-container">
+          <h1>Sign Up</h1>
+          <form action="">
+            <input type="text" name="firstName" placeholder="First Name" />
+            <input type="text" name="lastName" placeholder="Last Name" />
+            <input type="text" placeholder="Phone Number" name="mobile" />
+            <input type="email" name="email" placeholder="Email" />
+            <input type="password" name="password" placeholder="Password" />
+            <input type="password" name="confirmPassword" placeholder="Confirm Password" />
+            <button type="submit">Login</button>
+          </form>
+          <p>Already have an account? Sign In.</p>
+          <hr></hr>
+          <p>or</p>
+          <div className="oauth">
+            <button><i class="fab fa-facebook-square"></i>Login with Google</button>
+            <button><i class="fab fa-google"></i>Login with Facebook</button>
+          </div>
+        </div>
+        <img src={require("../../assets/market_woman1.jpg")} alt="Market woman" />
+      </div>
+    );
+  }
+}
+
+export default SignUp;
