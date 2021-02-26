aaf24edad852bcd5c25cebd7640068dded37ac3c
jothamardel <ardelmbiplang@gmail.com>
2021-02-27 00:14:06 +0100

    Updating firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -61,7 +61,7 @@ export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const signInWithGoogle = () => auth.signInWithRedirect(provider);
 
 const facebookProvider = new firebase.auth.FacebookAuthProvider();
 
